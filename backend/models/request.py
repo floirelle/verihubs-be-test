756593593076c73ae7f620dc788ea7dc15b5adad
floirelle <susantoadinata1@gmail.com>
2024-01-14 04:10:27 +0700

    use Optional instead

--- backend/models/request.py
+++ backend/models/request.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 class UserAuth(BaseModel):
     email: str
     password: str
-    password_confirmation: str | None = None
+    password_confirmation: Optional[str] = None
 
 class AddCoinRequest(BaseModel):
     coin_id: str
